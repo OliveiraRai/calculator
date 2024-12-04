67c34b47be916266e688f6f94a73c0774cce5d8e
raio <rai.conslog@hotmail.com>
2024-12-04 19:57:39 -0300

    added javascript

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function chooseKey(button){
+    const input = document.getElementById("input")
+    input.value += button.textContent;
+}
+
+function clearText(){
+    const input = document.getElementById("input")
+    input.value = "";
+}
+
+function calculate(){
+    const input = document.getElementById("input")
+    const expression = input.value;
+
+    try {
+        const result = eval(expression)
+        input.value = result;
+    } catch(error) {
+        input.value = "Error"
+    }
+}
